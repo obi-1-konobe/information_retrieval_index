8f51c960281b8c2b6badf4e6f353cd90f3877db2
obi-1-konobe <obi1konobe@gmail.com>
2020-03-08 00:28:32 +0300

    add comments to preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,7 +3,7 @@ import nltk
 import re
 import configs as c
 from nltk.corpus import stopwords
-
+# инициализация лексических библиотек
 morph = pymorphy2.MorphAnalyzer()
 nltk.download('stopwords')
 ru_stopwords = stopwords.words('russian')
@@ -13,12 +13,19 @@ class Preprocessing:
 
     @staticmethod
     def get_terms(doc_path):
+        """
+        преобразуем документ в массив термов
+        :param doc_path: путь к документу
+        :return: массив термов
+        """
         with open(doc_path, 'r', encoding='utf-8') as f:
             doc = f.readlines()
 
         terms = []
         for line in doc:
+            # сплитим строку по небуквенным символам
             tokens = re.split('\W', line)
+            # приводим слова к номальной форме
             terms += [morph.parse(token)[0].normal_form for token in tokens \
                       if token != '' \
                       and token not in ru_stopwords]
@@ -27,13 +34,17 @@ class Preprocessing:
 
     @staticmethod
     def get_query_terms(query_list):
+        """
+        получаем термы из строки запроса
+        :param query_list: токенизированная строка запроса
+        :return: термы запроса
+        """
         result_list = []
         for token in query_list:
+            # преобразуем слова, операторы не изменяем
             if token not in c.OPERATORS:
                 result_list.append(morph.parse(token)[0].normal_form)
             else:
                 result_list.append(token)
 
         return result_list
-
-
